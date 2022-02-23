435695e1df9506770b83629909ab60581c7a4705
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2022-02-23 10:59:14 +0100

    fix the extraction code

--- get_nucl_data/core.py
+++ get_nucl_data/core.py
@@ -38,14 +38,23 @@ class GetData():
         return str(self.path)
 
     def extract_nucl_data(self):
-        if self.path.exists():
-            print(f'Info: extracting nuclear cross sections into {self.dirname}')
-            results.sp.run(['tar','-xf','--verbose',self.path],capture_output=True,text=True)
-            s=results.split()
-            print(f'info: extracted {len(s)} .h5-files from {s[0]} to {s[-1]}')
-        #store the base extracted path - for instance to set the OPENMC_CROSS_SECTIONS env. var.
-        self.extract_path=pl.Path(s[0]).parent()
-        return str(self.extract_path)
+        try:
+            p=self.extracted_path
+            return str(self.path.parent / p)
+        except:
+            if self.path.exists():
+                print(f'Info: extracting nuclear cross sections into {self.dirname}')
+                results=sp.run(['tar','--verbose','--directory',self.dirname,'-xf',self.path],capture_output=True,text=True)
+                try:
+                    s=results.stdout.split()
+                except:
+                    return None
+                print(f'info: extracted {len(s)} .h5-files from {s[0]} to {s[-1]}')
+                #store the base extracted path - for instance to set the OPENMC_CROSS_SECTIONS env. var.
+                self.extracted_path=pl.Path(s[0])
+                return str(self.path.parent / self.extracted_path)
+            else:
+                return None
 
     def cleanup(self):
         self.path.unlink(missing_ok=True)
